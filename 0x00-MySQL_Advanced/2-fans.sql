52ab38a5376925d1f85d7255c2e930eb90c5b015
epicsociety <measkswho@gmail.com>
2023-10-12 19:41:19 +0000

    script that ranks country origins of bands, ordered by the number of (non-unique) fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,14 @@
 -- a SQL script
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 
-SELECT origin, SUM(nb_fans) AS total_fans, RANK() OVER (ORDER BY SUM(nb_fans) DESC) AS country_rank
-FROM metal_bands
-GROUP BY origin
-ORDER BY total_fans DESC;
+SELECT
+    origin,
+    SUM(nb_fans) AS total_fans,
+    (SELECT COUNT(DISTINCT origin) + 1
+	     FROM metal_bands AS subquery
+	     WHERE SUM(nb_fans) > (SELECT SUM(nb_fans) FROM metal_bands WHERE origin = subquery.origin)
+	    ) AS country_rank
+	FROM metal_bands
+	GROUP BY origin
+	ORDER BY total_fans DESC;
+
