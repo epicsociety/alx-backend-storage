3a6a347bb2e36f050b8243622ff191c307d03626
epicsociety <measkswho@gmail.com>
2023-10-12 19:11:15 +0000

    creates a table users with four columns

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Create a table users
+-- Create the table user with the following attributes
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+);
+
