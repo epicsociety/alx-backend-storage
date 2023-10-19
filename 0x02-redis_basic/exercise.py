3c347a0a4ef472f06eaea165007b3e7d00ca7aab
Epicsociety <measkswho@gmail.com>
2023-10-19 05:04:49 +0300

    convert the data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -21,3 +21,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:  # noqa: E501
+        """
+        Convert the data back to the desired format.
+        Args:
+            key: str
+            fn: callable
+        Returns: converted data
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Parametrize Cache.get with the correct conversion function"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """"Parametrize Cache.get with the correct conversion function"""
+        return self.get(key, fn=int)
